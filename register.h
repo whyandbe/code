f4c7cef2612555d2d58dfd58c9c0d40acbd901e0
whyandbe <1769357518@qq.com>
2025-12-16 17:55:20 +0800

    第二次提交

--- register.h
+++ register.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "node.h"
-void rg(){
-    fm fm1;
+mebr* rg(mebr* head){
     printf("请输入你的名字(小于10个字符)");
     char name1[10];
     scanf("%s",name1);
@@ -11,12 +10,34 @@ void rg(){
     printf("请输入你的密码(小于10个字符)");
     char passwords1[10];
     scanf("%s",passwords1);
-    strcpy(fm1.N,name1);
-    strcpy(fm1.U,username1);
-    strcpy(fm1.P,passwords1);
+    newaddp(head,name1,username1,passwords1,ping);
+    pingsavedata(head);
+    return head;
 }
-void savedate(){
-    FILE* fp = fopen("user.date","w");
-    fwrite(&fm,sizeof(fm),1,fp);s
-
+void pingsavedata(mebr* head){
+    FILE* fp = fopen("ping.user.date","w");
+    fwrite(head,sizeof(mebr),1,fp);
+    fclose(fp);
+}
+void applsavedata(mebr* head){
+    FILE* fp = fopen("appl.user.date","w");
+    fwrite(head,sizeof(mebr),1,fp);
+    fclose(fp);
+}
+mebr* pingreaddata(){
+    mebr* pinghead =createm();
+    FILE* fp =fopen("ping.user.date","r");
+    fread(pinghead,sizeof(mebr),1,fp);
+    fclose(fp);
+    return pinghead;
+}
+mebr* applreaddata(){
+    mebr* applhead =createm();
+    FILE* Fp =fopen("appl.user.date","r");
+    fread(applhead,sizeof(mebr),1,Fp);
+    fclose(Fp); 
+    return applhead;
+}
+int choice(){
+    printf("");
 }
